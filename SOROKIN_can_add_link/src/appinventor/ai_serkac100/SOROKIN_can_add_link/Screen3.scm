f4f7e03dc8da3f70baaa4e9cf130ed9fafecfcee
serkac1000 <47816293+serkac1000@users.noreply.github.com>
2025-05-05 08:26:28 +0300

    Delete SOROKIN_can_add_link directory

--- SOROKIN_can_add_link/src/appinventor/ai_serkac100/SOROKIN_can_add_link/Screen3.scm
+++
@@ -1,4 +0,0 @@
-#|
-$JSON
-{"authURL":["ai2.appinventor.mit.edu"],"YaVersion":"232","Source":"Form","Properties":{"$Name":"Screen3","$Type":"Form","$Version":"31","BackgroundColor":"&HFF0000FF","ActionBar":"True","AppName":"test4screenPODVODKIF1","Title":"LIST2","Uuid":"0"}}
-|#
--- SOROKIN_can_add_link/src/appinventor/ai_serkac100/SOROKIN_can_add_link/Screen4.scm
+++
@@ -1,4 +0,0 @@
-#|
-$JSON
-{"authURL":["ai2.appinventor.mit.edu"],"YaVersion":"232","Source":"Form","Properties":{"$Name":"Screen4","$Type":"Form","$Version":"31","BackgroundColor":"&HFF0000FF","ActionBar":"True","AppName":"FOOT01TEXNIKA","Title":"LIST03","Uuid":"0"}}
-|#
