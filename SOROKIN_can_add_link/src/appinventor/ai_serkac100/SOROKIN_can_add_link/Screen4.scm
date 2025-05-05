f701ee636eb2134ed46e97342ddf65d9f9b8858c
serkac1000 <47816293+serkac1000@users.noreply.github.com>
2025-05-05 07:54:04 +0300

    Add files via upload

---
+++ SOROKIN_can_add_link/src/appinventor/ai_serkac100/SOROKIN_can_add_link/Screen4.scm
@@ -0,0 +1,4 @@
+#|
+$JSON
+{"authURL":["ai2.appinventor.mit.edu"],"YaVersion":"232","Source":"Form","Properties":{"$Name":"Screen4","$Type":"Form","$Version":"31","ActionBar":"True","AppName":"FOOT01TEXNIKA","Title":"LIST03","Uuid":"0"}}
+|#
